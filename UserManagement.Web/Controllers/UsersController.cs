a6dc367f4e94c13a764a74c1f7a0e29630b44a1f
tyler <tyler@example.com>
2025-08-21 21:06:28 +0100

    Stage 2 added DOB to current relevant places before next steps

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -30,7 +30,8 @@ public class UsersController : Controller
             Forename = p.Forename,
             Surname = p.Surname,
             Email = p.Email,
-            IsActive = p.IsActive
+            IsActive = p.IsActive,
+            DateOfBirth = p.DateOfBirth
         });
 
         var model = new UserListViewModel
