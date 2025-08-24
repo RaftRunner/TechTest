4b8b90d9fb860e9e6b5aa71f3b92ad542d342f3d
tyler <tyler@example.com>
2025-08-24 23:41:52 +0100

    merge fixes

--- UserManagement.Web.Tests/UserControllerTests.cs
+++ UserManagement.Web.Tests/UserControllerTests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
 using UserManagement.Data.Entities;
 using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
